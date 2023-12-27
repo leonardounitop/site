510e6a138c69d4fe454a5699ac76143208acf9f3
Leonardo <leonardounitop@gmail.com>
2023-12-27 09:39:54 -0400

    contador

---
+++ anima-scroll.js
@@ -0,0 +1,24 @@
+export default function initAnimaScroll() {
+  const contents = document.querySelectorAll('[data-anime="scroll"]');
+
+  if (contents.length) {
+    const windowMetade = window.innerHeight * 0.8;
+
+    function animaScroll() {
+      contents.forEach((item) => {
+        const sectionTop = item.getBoundingClientRect().top;
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) {
+          item.classList.add('ativo');
+          console.log('visivel');
+        }
+      });
+    }
+
+    animaScroll();
+
+    window.addEventListener('scroll', animaScroll, { passive: true });
+  }
+}
+
+initAnimaScroll();
