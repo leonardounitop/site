4cdae2c7af9f92b7b89d9c8ecd66d0f027968c63
Leonardo <leonardounitop@gmail.com>
2023-12-28 10:01:39 -0400

    pagina sistema

--- anima-numero.js
+++ anima-numero.js
@@ -1,44 +1,46 @@
 export default function initAnimaNumeros() {
   const numeros = document.querySelectorAll('[data-numero]');
 
-  if (numeros) {
+  if (numeros.length) {
     numeros.forEach((numero) => {
       numero.innerText = 0;
     });
 
-    function animaNumeros() {
-      numeros.forEach((numero) => {
-        const total = +numero.dataset.numero;
+    if (numeros.length) {
+      function animaNumeros() {
+        numeros.forEach((numero) => {
+          const total = +numero.dataset.numero;
 
-        const incremento = Math.ceil(total / 100);
-        let start = 0;
+          const incremento = Math.ceil(total / 100);
+          let start = 0;
 
-        const timer = setInterval(() => {
-          start += incremento;
-          numero.innerText = start;
+          const timer = setInterval(() => {
+            start += incremento;
+            numero.innerText = start;
 
-          if (start > total) {
-            numero.innerText = total;
-            clearInterval(timer);
-          }
-        }, 30);
-      });
-    }
-
-    function handleMutation(mutation) {
-      const mudou = mutation[0].target.classList.contains('ativo');
-      if (mudou) {
-        observer.disconnect();
-        animaNumeros();
+            if (start > total) {
+              numero.innerText = total;
+              clearInterval(timer);
+            }
+          }, 30);
+        });
       }
+
+      function handleMutation(mutation) {
+        const mudou = mutation[0].target.classList.contains('ativo');
+        if (mudou) {
+          observer.disconnect();
+          animaNumeros();
+        }
+      }
+
+      const observerTarget = document.querySelector('.contador');
+      const observer = new MutationObserver(handleMutation);
+
+      console.log(observerTarget);
+
+      observer.observe(observerTarget, { attributes: true });
     }
-
-    const observerTarget = document.querySelector('.contador');
-    const observer = new MutationObserver(handleMutation);
-
-    console.log(observerTarget);
-
-    observer.observe(observerTarget, { attributes: true });
   }
 }
 
